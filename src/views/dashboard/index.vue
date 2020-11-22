0edc33b16536b457ff18aaf5cc1fde6f4c240dae
renyakun <renyakun@vchangyi.com>
2020-11-22 14:43:54 +0800

    init

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="dashboard">
+
+    <div class="jumbotron">
+      <h1 class="display-4">生活中常见的编码</h1>
+      <p class="lead">然而摩尔斯电码是由美国人摩尔斯在1837年被发明的，它的组成是由点dot（.）划dash（-）这两种符号所组成的。</p>
+      <hr class="my-4">
+      <img src="../../assets/imgs/mose.jpeg"/>
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+<!--    <el-carousel :interval="4000" height="800px" >-->
+<!--      <el-carousel-item>-->
+<!--        <img src="../../assets/imgs/mos.jpeg"/>-->
+<!--        <h3 class="medium">1</h3>-->
+<!--      </el-carousel-item>-->
+<!--      <el-carousel-item >-->
+<!--        <img src="../../assets/imgs/bra.jpg"/>-->
+<!--        <h3 class="medium">2</h3>-->
+<!--      </el-carousel-item>-->
+<!--      <el-carousel-item >-->
+<!--        <img src="../../assets/imgs/mos.jpeg"/>-->
+<!--        <h3 class="medium">3</h3>-->
+<!--      </el-carousel-item>-->
+<!--      <el-carousel-item >-->
+<!--        <img src="../../assets/imgs/mos.jpeg"/>-->
+<!--        <h3 class="medium">4</h3>-->
+<!--      </el-carousel-item>-->
+<!--    </el-carousel>-->
+
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  onload() {
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  min-height: 100vh;
+  .jumbotron{
+    min-height: 100vh;
+    margin-bottom: 0;
+  }
+
+}
+</style>
